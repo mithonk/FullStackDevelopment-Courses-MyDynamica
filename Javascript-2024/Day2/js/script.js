011a68cc7231fb0650005c19f674f981c2569158
mithonk <mitho9798@gmail.com>
2024-05-20 10:43:29 +0530

    20.05.2024

---
+++ Javascript-2024/Day2/js/script.js
@@ -0,0 +1,127 @@
+//alert('Welcome to Javascript');
+
+// var x = 10;
+
+// console.log(x);
+// console.error(x);
+// console.warn(x);
+
+// var x;
+
+// x = 10;
+// console.log(x);
+
+// x = 20;
+// console.log(x);
+
+// let x = 10;
+// console.log(x);
+
+// let x = 50;
+// console.log(x);
+
+// const x = 10;
+
+// console.log(x);
+
+// x = 20;
+// console.log(x);
+
+// console.log(x);
+
+// var x = 10;
+
+// var x = 10;
+// var y = '20';
+
+// console.log(typeof x);
+// console.log(typeof y);
+
+// var boo = true;
+// console.log(typeof boo);
+// console.log(typeof(boo));
+
+// var fname = 'Mithursan';
+// var boo = true;
+
+// console.log(fname, typeof fname, boo, typeof boo);
+
+//String to Number
+
+// var x = '50';
+
+// console.log(x, typeof x);
+
+// // x = Number(x);
+// //x = +x;
+// x = parseInt(x);
+
+// console.log(x, typeof x);
+
+// var x = '50.9';
+
+// console.log(x, typeof x);
+
+// // x = Number(x);
+// //x = +x;
+// x = parseFloat(x);
+
+// console.log(x, typeof x);
+
+//Number to String
+
+// var x = 50;
+
+// console.log(x, typeof x);
+
+// //x = String(x);
+// // x='x';
+// x = x.toString();
+
+// console.log(x, typeof x);
+
+//Number to boolean
+
+// var x = 50.9;
+
+// console.log(x, typeof x);
+
+// x = Boolean(x);
+
+// console.log(x, typeof x);
+
+// var x = 0;
+// x = Boolean(x);
+
+// if (x) {
+//   console.log('this is not zero');
+// } else {
+//   console.log('this is zero');
+// }
+
+//Arrays
+
+// let y = [10, 20, 30, 40, 50];
+
+// console.log(y, typeof y);
+
+// let z = {
+//   name: 'Mithursan',
+//   age: 26,
+// };
+
+// console.log(z, typeof z);
+
+//Function
+
+// let a = function () {
+//   console.log('This is a function');
+// };
+
+// console.log(a, typeof a);
+
+// let x;
+// console.log(x, typeof x);
+
+let x = null;
+console.log(x, typeof x);

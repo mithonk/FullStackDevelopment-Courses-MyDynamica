68a49b85059da543f4088a9031afcced87435267
mithonk <mitho9798@gmail.com>
2024-05-23 10:56:30 +0530

    23.05.2024

---
+++ Javascript-2024/Day3/script.js
@@ -0,0 +1,141 @@
+//console.log('test');
+
+/*
+let fname = 'Mithursan';
+console.log('First Name : ', fname);
+let lname = fname;
+console.log('Last Name : ', lname);
+lname = 'Kalanantharasan';
+console.log('Last Name : ', lname);
+console.log('First Name: ', fname);
+*/
+
+/*
+let fname = {
+  name: 'Mithursan',
+};
+console.log('First Name : ', fname.name);
+let lname = fname;
+console.log('Last Name : ', lname.name);
+lname.name = 'Kalanantharasan';
+console.log('Last Name : ', lname.name);
+console.log('First Name : ', fname.name);
+
+fname.name = 'Tamil';
+console.log('First Name : ', fname.name);
+console.log('Last Name : ', lname.name);
+*/
+
+/*
+let a, b, c;
+a = 10;
+b = 20;
+c = 30;
+
+console.log(a, b, c);
+
+const d = 40,
+  e = 50,
+  f = 60;
+
+console.log(d, e, f);
+*/
+
+/*
+let a = 10,
+  b = 20,
+  c = 30;
+console.log(a, b, c);
+*/
+
+/*
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+*/
+
+/*
+let fname = 'Mithursan';
+let lname = 'Kalanantharasan';
+
+console.log(fname + ' ' + lname);
+console.log(fname, ' ', lname);
+console.log(fname, lname);
+*/
+
+/*
+let x = 10;
+
+console.log(x);
+//x = x + 5;
+x += 5;
+console.log(x);
+x -= 3;
+console.log(x);
+x *= 2;
+console.log(x);
+x /= 3;
+console.log(x);
+x %= 4;
+console.log(x);
+*/
+
+/*
+let x = 2;
+
+console.log(x); //2
+x++;
+console.log(x); //3
+++x;
+console.log(x); //4
+x--;
+console.log(x); //3
+--x;
+console.log(x); //2
+
+console.log(x++); //2
+console.log(x); //3
+console.log(++x); //4
+console.log(x); //4
+console.log(x--); //4
+console.log(x); //3
+console.log(--x); //2
+*/
+
+/*
+let x = 2;
+
+x = x ** 3;
+console.log(x);
+*/
+
+/*
+let x = 5;
+let y = 3;
+
+console.log(x == y); //false
+console.log(x != y); //true
+console.log(x >= y); //true
+console.log(x <= y); //false
+console.log(x > y); //true
+console.log(x < y); //false
+*/
+
+/*
+let x = 5;
+let y = '5';
+
+console.log(x == y);
+console.log(x === y);
+*/
+
+let x = '5';
+let y = 10;
+
+console.log(x != y);
+console.log(x !== y);

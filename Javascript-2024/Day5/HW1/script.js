3949824ea021bc32bd691b1d1663a0e981658d19
mithonk <mitho9798@gmail.com>
2024-05-27 11:35:35 +0530

    27.05.2024

--- Javascript-2024/Day5/HW1/script.js
+++ Javascript-2024/Day5/HW1/script.js
@@ -3,9 +3,25 @@ function myFunction() {
   let nicNo = document.getElementById('nicNo').value;
   //   console.log(nicNo);
 
+  /*
   if (
     nicNo.length == 10 &&
     nicNo.substring(0, 9) >= 0 &&
+    (nicNo.endsWith('x') || nicNo.endsWith('v'))
+  ) {
+    document.getElementById(
+      'msg'
+    ).innerText = `Your NIC Number ${nicNo} is Correct`;
+  } else {
+    document.getElementById(
+      'msg'
+    ).innerText = `Your NIC Number ${nicNo} is Wrong. Plese Check Your NIC Number.`;
+  }
+  */
+
+  if (
+    nicNo.length == 10 &&
+    !isNaN(nicNo.substring(0, 9)) &&
     (nicNo.endsWith('x') || nicNo.endsWith('v'))
   ) {
     document.getElementById(

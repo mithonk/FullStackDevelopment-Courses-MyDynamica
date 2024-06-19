42a01fcc88f8afd426a4f73c42d4d265af90ad73
mithonk <mitho9798@gmail.com>
2024-06-19 09:04:00 +0530

    19-06-2024

---
+++ Javascript-2024/Day8/HW/script.js
@@ -0,0 +1,32 @@
+document.getElementById('add').addEventListener('click', () => add());
+document.getElementById('sub').addEventListener('click', () => sub());
+document.getElementById('mul').addEventListener('click', () => mul());
+document.getElementById('div').addEventListener('click', () => div());
+
+const add = () => {
+  let num1 = document.getElementById('number1').value;
+  let num2 = document.getElementById('number2').value;
+  let output = Number(num1) + Number(num2);
+  document.getElementById('result').innerText = output;
+};
+
+sub = () => {
+  let num1 = document.getElementById('number1').value;
+  let num2 = document.getElementById('number2').value;
+  let output = Number(num1) - Number(num2);
+  document.getElementById('result').innerText = output;
+};
+
+mul = () => {
+  let num1 = document.getElementById('number1').value;
+  let num2 = document.getElementById('number2').value;
+  let output = Number(num1) * Number(num2);
+  document.getElementById('result').innerText = output;
+};
+
+div = () => {
+  let num1 = document.getElementById('number1').value;
+  let num2 = document.getElementById('number2').value;
+  let output = Number(num1) / Number(num2);
+  document.getElementById('result').innerText = output;
+};

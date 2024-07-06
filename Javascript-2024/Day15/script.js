dc124b1a7a809b71b27185adf96e5431bfa6ee0e
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-07-06 14:47:14 +0530

    06-07-2024

--- Javascript-2024/Day15/script.js
+++ Javascript-2024/Day15/script.js
@@ -1,1 +1,27 @@
-console.log('Test');
+// console.log('Test');
+
+/*
+class Student {
+  SetfName(fname) {
+    this.fname = fname;
+  }
+  getfName() {
+    console.log(this.fname);
+  }
+}
+
+let student1 = new Student();
+student1.SetfName('Mithursan');
+student1.getfName();
+*/
+
+class Student {
+  set fName(fname) {
+    this.fname = fname;
+  }
+  get fName() {
+    console.log(this.fname);
+  }
+}
+
+let student1 = new Student();

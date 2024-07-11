fac2e409b846d918e74570bf12e16ecf96253a0c
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-07-11 10:10:21 +0530

    11-07-2024

--- NodeJS-2024/Day21/index.js
+++ NodeJS-2024/Day21/index.js
@@ -10,4 +10,30 @@ console.log(obj);
 */
 
 // alert('Tamil');
-console.log('Wel Come');
+
+// console.log('Wel Come');
+
+/*
+// import { PrintName } from './student.js';
+const stu = require('./student.js');
+
+stu.PrintName();
+stu.display();
+*/
+/*
+const { PrintName, display } = require('./student.js'); //Object Destructuring
+
+PrintName();
+display();
+*/
+/*
+import { PrintName, display } from './student.js';
+
+PrintName();
+display();
+*/
+// import fs from 'fs';
+// console.log(fs);
+
+// const fs = require('fs');
+// console.log(fs);
--- NodeJS-2024/Day21/student.js
+++ NodeJS-2024/Day21/student.js
@@ -1,3 +1,13 @@
 function PrintName() {
-  console.log('My Name is Mithursancd');
+  console.log('My Name is Mithursan');
 }
+
+// PrintName();
+
+function display() {
+  console.log('My Firstname is Mithursan');
+  console.log('My Lastname is Kalanantharasan');
+  console.log('My Fullname is Mithursan Kalanantharasan');
+}
+
+module.exports = { PrintName, display };
